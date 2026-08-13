8ce3a670b5998cd4f8eb74278c39893a517b2cdf
BalabanKhan <BalabanKhan@users.noreply.github.com>
2026-08-13 18:16:21 +0300

    balance: Calibrate side business economy to 30-40 day ROI payback curve

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"14c05c42a5556a515deea59a1e93553f10575c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1454167168" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "671217838" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
