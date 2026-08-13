05fd3ae1c39863aaab34c38f92c46a8ba6f020df
BalabanKhan <BalabanKhan@users.noreply.github.com>
2026-08-13 14:54:29 +0300

    build: Web surumu guncellendi (flutter build web)

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"14c05c42a5556a515deea59a1e93553f10575c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3768526560" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "927012761" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
