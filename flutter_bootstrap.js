b27cecc13e117e84d87f64a8c5a7ebaa777eef6d
BalabanKhan <BalabanKhan@users.noreply.github.com>
2026-08-14 13:09:05 +0300

    fix: overhaul isometric showroom painter layout and road positioning

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"14c05c42a5556a515deea59a1e93553f10575c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1040889685" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4257887849" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
