f18de1a39e9db454d6f857d6f317b688cf20051c
BalabanKhan <BalabanKhan@users.noreply.github.com>
2026-08-14 13:33:58 +0300

    fix: remove 2.5D text header, cleanup red HUD pills, and optimize dashboard layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"14c05c42a5556a515deea59a1e93553f10575c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3556357154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "239637579" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
